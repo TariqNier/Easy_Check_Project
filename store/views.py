cca8aa35736363fd14bfb3f97c4b77ac27389ee3
TariqNier <tariq0elyousfy@gmail.com>
2025-12-25 19:33:40 +0200

    Added transaction.atomic for payment logic

--- store/views.py
+++ store/views.py
@@ -2,9 +2,8 @@
 import datetime
 import requests
 from django.conf import settings
-from django.shortcuts import redirect
 from django.contrib.auth import get_user_model
-
+from django.db import transaction
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -39,37 +38,38 @@ class TransactionViewSet(viewsets.ModelViewSet):
         
         if user and serializer.validated_data.get('service_details'):
             amount = serializer.validated_data.get('amount')
-        
-            if user.balance < amount:
-                return Response({"error":"Insufficient balance",'Current Balance':user.balance}, status=status.HTTP_400_BAD_REQUEST)
-        
-            user.balance -= amount
-            user.save()
             
-            transaction = serializer.save(user=user, status='COMPLETED', is_balance_topup=False)
+            with transaction.atomic():
+                if user.balance < amount:
+                    return Response({"error":"Insufficient balance",'Current Balance':user.balance}, status=status.HTTP_400_BAD_REQUEST)
             
-            imei = transaction.service_details.get('imei')
+                user.balance -= amount
+                user.save()
+            
+                transaction_obj = serializer.save(user=user, status='COMPLETED', is_balance_topup=False)
+            
+            imei = transaction_obj.service_details.get('imei')
             print(f"(User) Triggering API for IMEI: {imei}")
             #hena hykoon el logic beta3 el imei service. 
             
             return Response({
                 "status": "success",
-                "transaction_id": transaction.id,
-                "transaction_status": transaction.status,
+                "transaction_id": transaction_obj.id,
+                "transaction_status": transaction_obj.status,
                 "new_balance": user.balance,
             }, status=status.HTTP_201_CREATED)
             
-        transaction = serializer.save(user=user)
+        transaction_obj = serializer.save(user=user)
         
         
         payload = {
             "expireAt": str((datetime.datetime.now() + datetime.timedelta(minutes=30)).isoformat() + "Z"),
             "maxFailureAttempts": 3,
-            "amount": str(transaction.amount),
+            "amount": str(transaction_obj.amount),
             "currency": "EGP",
             "merchantId": settings.KASHIER_MID,
             #"mode":"test",
-            "order": str(transaction.merchant_transaction_id),
+            "order": str(transaction_obj.merchant_transaction_id),
             "merchantRedirect": "http://localhost:5173/",
             "display": "en",
             "paymentType": "credit",
@@ -78,7 +78,7 @@ class TransactionViewSet(viewsets.ModelViewSet):
             "allowedMethods": "card,wallet,fawry,instapay,basata",
             "customer": {
                 "name": str(user.phone_number) if user else "Guest",
-                "email": "",
+                "email": "user@example.com", 
                 "reference": str(user.id) if user else "guest"
             }
         }
@@ -100,26 +100,26 @@ class TransactionViewSet(viewsets.ModelViewSet):
                     kashier_id = response_data.get('_id') or response_data.get('kashierOrderId')
                     
                     if kashier_id:
-                        transaction.kashier_session_id = kashier_id
-                        transaction.save()
+                        transaction_obj.kashier_session_id = kashier_id
+                        transaction_obj.save()
                     
                     # 3. RETURN TO FRONTEND
                     return Response({
                         "status": "success",
                         "paymentUrl": payment_url, 
-                        "transaction_id": transaction.id,
-                        "merchant_transaction_id": str(transaction.merchant_transaction_id)
+                        "transaction_id": transaction_obj.id,
+                        "merchant_transaction_id": str(transaction_obj.merchant_transaction_id)
                     }, status=status.HTTP_201_CREATED)
             
             print("Kashier Error Response:", response_data)
-            transaction.status = 'FAILED'
-            transaction.save()
+            transaction_obj.status = 'FAILED'
+            transaction_obj.save()
             return Response(response_data, status=response.status_code)  
         
         except requests.exceptions.RequestException as e:
             print("Kashier Exception:", str(e))
-            transaction.status = 'FAILED'
-            transaction.save()
+            transaction_obj.status = 'FAILED'
+            transaction_obj.save()
             return Response({"error":"Kashier service unreachable"}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
           
     @action(detail=False, methods=['post'], url_path='webhook/kashier')
@@ -136,93 +136,39 @@ class TransactionViewSet(viewsets.ModelViewSet):
 
         
         try:
-            transaction = Transaction.objects.get(merchant_transaction_id=transaction_id)
+            transaction_obj = Transaction.objects.get(merchant_transaction_id=transaction_id)
         except Transaction.DoesNotExist:
             return Response({"error": "Not Found"}, status=status.HTTP_404_NOT_FOUND)
 
        
-        if transaction.status == 'COMPLETED':
+        if transaction_obj.status == 'COMPLETED':
             return Response({"status": "already_processed"}, status=status.HTTP_200_OK)
-
+        
         if payment_status == "SUCCESS":
-            transaction.status = 'COMPLETED'
+            with transaction.atomic():
             
-            
-            if 'orderId' in webhook_data:
-                transaction.kashier_session_id = webhook_data['orderId']
-            
-            transaction.save()
-            print(f"Payment Verified: {transaction_id}")
+                transaction_obj.status = 'COMPLETED'
+                
+                
+                if 'orderId' in webhook_data:
+                    transaction_obj.kashier_session_id = webhook_data['orderId']
+                
+                transaction_obj.save()
+                print(f"Payment Verified: {transaction_id}")
 
-            if transaction.is_balance_topup and transaction.user:
-                transaction.user.balance += transaction.amount
-                transaction.user.save()
-                print(f"User {transaction.user.id} balance topped up by {transaction.amount}. New balance: {transaction.user.balance}")
-            elif transaction.service_details:
-                imei = transaction.service_details.get('imei')
+                if transaction_obj.is_balance_topup and transaction_obj.user:
+                    transaction_obj.user.balance += transaction_obj.amount
+                    transaction_obj.user.save()
+                    print(f"User {transaction_obj.user.id} balance topped up by {transaction_obj.amount}. New balance: {transaction_obj.user.balance}")
+            
+            if transaction_obj.service_details and not transaction_obj.is_balance_topup:
+                imei = transaction_obj.service_details.get('imei')
                 print(f"(Guest) Triggering Unlocking API for IMEI: {imei}")
                 
         
         else:
-            transaction.status = 'FAILED'
-            transaction.save()
+            transaction_obj.status = 'FAILED'
+            transaction_obj.save()
             print(f"Payment Failed for: {transaction_id}")
 
-        return Response({"status": "received"}, status=status.HTTP_200_OK)        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    # def create(self,request, *args, **kwargs):
-    #     serializer= self.get_serializer(data=request.data, context={'request': request})
-    #     serializer.is_valid(raise_exception=True)
-        
-    #     if request.user.is_authenticated:
-    #         user=request.user
-    #     else:
-    #         user=None
-        
-    #     transaction = serializer.save(user=user)
-        
-    #     payload = {
-    #         "amount": str(transaction.amount),
-    #         "currency": "EGP",
-    #         "merchant_transaction_id": str(transaction.merchant_transaction_id),
-    #         "operation": "purchase",
-    #         "customerName": user.username if user else "Guest",
-    #     }
-        
-    #     try:
-    #         url = f"{settings.KASHIER_API_URL}/payment/session"
-    #         headers = get_kashier_auth_headers()
-    #         response = requests.post(url, json=payload, headers=headers)
-    #         response_data = response.json()
-            
-    #         if response.status_code in [200,201]:
-    #             return Response({
-    #                 "sessionToken": response_data.get("sessionToken"),
-    #                 "merchantTransactionId": str(transaction.merchant_transaction_id),
-    #                 "id": transaction.id
-    #             }, status=status.HTTP_201_CREATED)
-            
-    #         return Response(response_data, status=response.status_code)
-    
-    #     except requests.exceptions.RequestException as e:
-    #         return Response({"error":"Kashier service unreachable"}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
-    
-    
-    
+        return Response({"status": "received"}, status=status.HTTP_200_OK)
